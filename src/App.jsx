8857a8e5db87d71e24ba59ee52eb64044619b36a
Gideon Abel <gideonabel87@gmail.com>
2024-05-06 15:47:22 +0100

    box shadow added

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,86 @@
 
+// css file
 import "./App.css";
+// icons
 import { IoLogOutOutline } from "react-icons/io5";
 import { FaRegEdit } from "react-icons/fa";
 import { RiDeleteBin4Line } from "react-icons/ri";
+// end of icons
+
+
+import Header from "./components/Header"
 
 function App() {
 
   return (
-    <body className='App'>
-      <main>
-          {/* header section */}
+    <div>
+      <body className='App'>
+        <main>
+            {/* Header */}
             <header>
-              {/* logo section */}
               <section>
+                {/* logo section */}
                 <h3>SEFE<span>TODO</span></h3>
               </section>
-              {/* logout icon section */}
+                {/* logout icon section */}
               <section className="logout">
-              <IoLogOutOutline />
+                <IoLogOutOutline />
               </section>
             </header>
-            {/* body section */}
+            {/* Header ends */}
+              {/* body section */}
             <div className="second-section">
-              <section className="container">
-                <div>
-                  <h2>Todo Done</h2>
-                  <p>keep it up</p>
-                </div>
-                <div className="circle">
-                  <h3>1/3</h3>
-                </div>
-              </section>
-              <section className="input-1">
-                <div className="text-area">
-                  <input type="text" placeholder="write your next task" />
-                  <div className="plus">+</div>
-                </div>
-                <div className="task-1">
+                <section className="container">
                   <div>
-                    <input type="radio" id="task-1" name="task-1" />
-                    <label htmlFor="task-1">Task 1</label>
+                    <h2>Todo Done</h2>
+                    <p>keep it up</p>
                   </div>
-                  <i>
-                    <FaRegEdit />
-                    <RiDeleteBin4Line />
-                  </i>
-                </div>
-                <div className="task-1">
-                  <div>
-                    <input type="radio" id="task-1" name="task-1" />
-                    <label htmlFor="task-1" className="task-2">Task 2</label>
+                  <div className="circle">
+                    <h3>1/3</h3>
                   </div>
-                  <i>
-                    <FaRegEdit />
-                    <RiDeleteBin4Line />
-                  </i>
-                </div>
-                <div className="task-1">
-                  <div>
-                    <input type="radio" id="task-1" name="task-1" />
-                    <label htmlFor="task-1">Task 2</label>
+                </section>
+                <section className="input-1">
+                  <div className="text-area">
+                    <input type="text" placeholder="write your next task" />
+                    <div className="plus">+</div>
                   </div>
-                  <i>
-                    <FaRegEdit />
-                    <RiDeleteBin4Line />
-                  </i>
-                </div>
-              </section>
-
-            </div>
-      </main>
-    </body>
+                  <div className="task-1">
+                    <div>
+                      <input type="radio" id="task-1" name="task-1" />
+                      <label htmlFor="task-1">Task 1</label>
+                    </div>
+                    <i>
+                      <FaRegEdit />
+                      <RiDeleteBin4Line />
+                    </i>
+                  </div>
+                  <div className="task-1">
+                    <div>
+                      <input type="radio" id="task-1" name="task-1" />
+                      <label htmlFor="task-1" className="task-2">Task 2</label>
+                    </div>
+                    <i>
+                      <FaRegEdit />
+                      <RiDeleteBin4Line />
+                    </i>
+                  </div>
+                  <div className="task-1">
+                    <div>
+                      <input type="radio" id="task-1" name="task-1" />
+                      <label htmlFor="task-1">Task 2</label>
+                    </div>
+                    <i>
+                      <FaRegEdit />
+                      <RiDeleteBin4Line />
+                    </i>
+                  </div>
+                </section>
+  
+              </div>
+        </main>
+      </body>
+    </div>
   )
 }
 
-export default App;
+export default App
